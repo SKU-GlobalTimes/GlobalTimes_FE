61e8a48cd01ba769da463fc3e98897be8489aafe
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-03-25 04:09:05 +0900

    ✨ Feat: 검색 결과 페이지 컴포넌트 배치

--- FrontEnd/src/mainPage/MainPage.jsx
+++ FrontEnd/src/mainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import styled from './MainPage.module.css';
 import SearchContainer from './SearchContainer';
 import MainNews from './MainNews';
+import SearchNews from './SearchNews';
 
 export default function MainPage() {
 
@@ -8,6 +9,7 @@ export default function MainPage() {
         <div className={styled['mainPage--container']}>
             <SearchContainer />
             <MainNews />
+            <SearchNews />
         </div>
     )
 }
---
+++ FrontEnd/src/mainPage/SearchNews.jsx
@@ -0,0 +1,29 @@
+import styled from './News.module.css';
+
+export default function SearchNews() {
+
+    return(
+        <div className={styled['SearchNews--container']}>
+            <h1>검색 결과</h1>
+            <div className={styled['SearchNews--resultList']}>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+                <div className={styled['news--sample03']}></div>
+            </div>
+        </div>
+    )
+}
+
