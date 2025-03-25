f2e028d100b73fb0784278c0133373dd6a480bf4
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-03-25 15:04:03 +0900

    :recycle: Refactor: mainPage 폴더 components폴더 안으로 이동하고, Router 연결

--- FrontEnd/src/router/Routers.jsx
+++ FrontEnd/src/router/Routers.jsx
@@ -2,7 +2,7 @@ import { Routes as ReactRouters, Route } from 'react-router-dom';
 
 // pages
 import LandingPage from '../components/landingPage/LandingPage';
-
+import MainPage from '../components/mainPage/MainPage';
 
 // layouts
 import MainLayout from '../layouts/MainLayout';
@@ -13,7 +13,7 @@ export default function Routes() {
         <ReactRouters>
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<LandingPage />} />
-            
+                <Route path='main' element={<MainPage />} />
             </Route>
         </ReactRouters>
     )
