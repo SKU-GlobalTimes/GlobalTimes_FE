05e20eaee8a54d5dd6de48b60c80753168b58d46
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-03-25 15:56:35 +0900

    ✨ Feat: Header - 메인페이지 클릭시 mainPage로 이동되도록 navigate

--- FrontEnd/src/components/commons/header/Header.jsx
+++ FrontEnd/src/components/commons/header/Header.jsx
@@ -1,11 +1,12 @@
-
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+    const navigate = useNavigate();
 
     return(
         <div>
             <p>GLOBAL TIMES</p>
-            <p>메인페이지</p>
+            <p onClick={()=> {navigate("/main");}}>메인페이지</p>
             <p>마이스크랩</p>
             <p>서비스 소개</p>
         </div>
