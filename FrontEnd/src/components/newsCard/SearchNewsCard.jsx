f3ab2caf5aa494d305d4ee1956f8d1d1f9dc048f
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-04-03 02:27:11 +0900

    :art: 검색결과 뉴스 카드 UI 수정

--- FrontEnd/src/components/newsCard/SearchNewsCard.jsx
+++ FrontEnd/src/components/newsCard/SearchNewsCard.jsx
@@ -1,15 +1,19 @@
 import styled from './NewsCard.module.css';
 import PropTypes from 'prop-types';
 
-export default function SearchNewsCard({ title, summary, image }) {
+export default function SearchNewsCard({ press, title, summary, image, year, month, day }) {
 
     return(
         <div className={styled['searchNewsCard--container']}>
             <div className={styled['searchNewsCard--contents']}>
-                <p className={styled['searchNewsCard--contents__title']}>{title}</p>
-                <p className={styled['searchNewsCard--contents__preview']}>
-                    {summary}
-                </p>
+                <div className={styled['searchNewsCard--contents__topContainer']}>
+                    <p className={styled['searchNewsCard--contents__press']}>{press}</p>
+                    <p className={styled['searchNewsCard--contents__title']}>{title}</p>
+                    <p className={styled['searchNewsCard--contents__preview']}>
+                        {summary}
+                    </p>
+                </div>
+                <p className={styled['searchNewsCard--contents__date']}>{year}.{month}.{day}</p>
             </div>
             <div className={styled['searchNewsCard--imageContainer']}>
                 <div 
@@ -22,8 +26,13 @@ export default function SearchNewsCard({ title, summary, image }) {
 }
 
 SearchNewsCard.propTypes = {
+    press: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
+    image: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    month: PropTypes.string.isRequired,
+    day: PropTypes.string.isRequired
 };
+
 
